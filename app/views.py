97d09392a9ceeac12ba9c3070d89e09b7099a3bb
mdayjas <mohammadayjaz99@gmail.com>
2024-09-21 17:44:10 +0530

    solved-2nd question

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 import time
+import threading
+
 
 
 def index(request):
@@ -15,9 +17,21 @@ def my_handler(sender, instance, **kwargs):
     time.sleep(5)  # Sleep to simulate time-consuming task
     print("Signal finished processing.")
 
+# 2.Do Django signals run in the same thread as the caller?
+# Yes, by default, Django signals run in the same thread as the caller, which means that signal handling occurs in the main thread unless explicitly offloaded to another thread.
+    print(f"Signal received in thread: {threading.current_thread().name}")
+    
 # # Creating a new user will trigger the signal
 try:
     User.objects.create(username='test_user')
 except Exception as e:
     print("Error creating user:", str(e))
 print("After User creation.")
+
+
+
+
+
+# Creating a user and observing the thread
+# print(f"Main thread: {threading.current_thread().name}")
+# User.objects.create(username='test_user')
